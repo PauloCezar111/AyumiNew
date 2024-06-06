45bf1735530c78523ddcd6f792bb59dfa14ad030
AyumiBotOficial <167991708+AyumiBotOficial@users.noreply.github.com>
2024-06-06 14:33:56 -0300

    Nova Atualização Beta 1.0

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "${RED}[ ! ] INICIANDO O BOT, AGUARDE!"
 echo "${RED}[ ! ] CASO DESEJA COMPRAR A VERSÃO DESCRIPTOGRAFADA!"
 echo "${RED}[ ! ] ENTRE EM CONTATO COM MEU CRIADOR!"
 echo "${RED}[ ! ] +55 61 9669-7514"
-    node start.js --code 
+    node index.js --code 
     sleep 1
 
 done
